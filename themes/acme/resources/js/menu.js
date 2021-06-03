48fb861ff2d08fd4a1fe004dd526e144ba72c22a
Derek Bell <lucid@lucid.com.au>
2021-06-03 21:42:06 +1000

    Mobile menu

---
+++ themes/acme/resources/js/menu.js
@@ -0,0 +1,8 @@
+const menuButton = document.querySelector('#menuButton');
+const menuList = document.querySelector('#menuList');
+
+function toggleMenu(){
+    menuList.classList.toggle("nav-menu--is-open");
+}
+
+menuButton.addEventListener('click', toggleMenu);
